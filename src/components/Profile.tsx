f21e724f018f70893a59e01842ef76e5c3d7bb67
Conrado Alberto da Silva Stochi <stochiconrado@gmail.com>
2023-11-24 16:55:08 -0300

    Full Project NLW04-MoveIt

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+
+  const { level } = useContext(ChallengesContext)
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/guilhermecapitao.png" alt="Guilherme Capitão"/>
+      <div>
+        <strong>Guilherme Capitão</strong>
+        
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level {level}
+        </p>
+      </div>
+    </div>
+  )
+}
